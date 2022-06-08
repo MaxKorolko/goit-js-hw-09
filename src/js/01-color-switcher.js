326f11df989dbb64415a80c0a79e0fa34874991d
MaxKorolko <m.v.korolko@gmail.com>
2022-06-08 15:18:57 +0300

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+import getRandomHexColor from './random-color';
 
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+
+refs.startButton.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  refs.startButton.setAttribute('disabled', true);
+});
+
+refs.stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.startButton.removeAttribute('disabled');
+});
