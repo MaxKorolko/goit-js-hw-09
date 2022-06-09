6c2a7f4e182a6d04b02f1a09e694a512458773e8
MaxKorolko <m.v.korolko@gmail.com>
2022-06-10 00:07:17 +0300

    Promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', conclusionPromises);
+
+function conclusionPromises(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = getFormData(event.currentTarget);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, step * i)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+  event.currentTarget.reset();
+}
+
+function getFormData(el) {
+  formData = {};
+
+  new FormData(el).forEach((value, name) => (formData[name] = Number(value)));
+  return formData;
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
